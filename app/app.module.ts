1dcbf42935c040de8bc0acb19cc43782d7ba68c4
Dell <SUBHADIP@Dell>
2017-04-08 09:24:42 +0530

    parent child comp

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
-
 import { StringModi } from './stringModi.pipe';
 
+import {ParentComp} from './parent.component';
+import {ChildComp} from './child.component';
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, StringModi ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, StringModi, ParentComp, ChildComp ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
